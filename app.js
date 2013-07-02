e72c11bef3f0ed7c3e2f56ed890ff7eff2fcaf7f
Andrew Barba <abarba.77@gmail.com>
2013-07-01 22:28:56 -0400

    add app.js

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
+/***
+ ** This is the Suprizer backend API
+ ** 2013 Suprizr Inc.
+ **/
+
+/**
+ * Initialize Express
+ */
 var express = require("express");
 var app = express();
-app.use(express.logger());
+app.configure(function () {
+	app.use(express.logger());
+	app.use(express.compress()); // GZIP data
+	app.use(express.methodOverride()); // allow PUT and DELETE
+ 	app.use(express.bodyParser()); // JSON post body
+ 	app.use(express.cookieParser()); // JSON cookies
+ 	app.use(app.router);
+});
 
+/**
+ * Initialize Mongoose and connect to MongoHQ
+ */
 var mongoose = require("mongoose");
 mongoose.connect(process.env.MONGOHQ_URL);
 
-app.get('/', function(request, response) {
-	response.send('Hello World!');
-});
+/**
+ * Initialize custom routing
+ */
+var suprizr = require("suprizr");
 
+/**
+ * Start the server
+ */
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
  	console.log("Listening on " + port);
